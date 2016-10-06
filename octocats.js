f353c9cfe8f5af1a24d8c1070c47ab09f22b0a3b
JackDunne <8jdunne@stpatrick.org>
2016-10-06 11:53:55 -0500

    we made octocat objects

---
+++ octocats.js
@@ -0,0 +1,24 @@
+//Write your constructor function below.
+function Octocat(n,a){
+this.Name = n;
+this.Arms = a;
+
+var Greg = new Octocat("Greg",8);
+var Jack = new Octocat("Jack",69);
+var Pabloescobar = new Octocat("Pablo Escobar",17);
+
+  for(var b=0;b<Arms;b++){
+    console.log("SLAP");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
